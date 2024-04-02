d569bf341467548cbb442a123427f8fddbb34b04
jopopapi <106134415+jopopapi@users.noreply.github.com>
2024-04-02 16:48:12 +0900

    broswer updates

--- browsertabs.py
+++ browsertabs.py
@@ -1,10 +1,12 @@
 import pychrome
 import subprocess
 import os
+from selenium import webdriver
+import time
 
-def init():
+def pychr():
 
-    os.system("Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222")
+    #os.system("Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222")
     browser = pychrome.Browser(url="http://127.0.0.1:9222")
     
     # Get the list of tabs
@@ -13,4 +15,21 @@ def init():
     #close the connection
     browser.close_tab()
 
-init()
+
+# seleniu is more of an automation tool, but not good for tabs that are already open
+def selen():
+    driver = webdriver.Chrome()
+
+    driver.execute_script("window.open('');")
+    driver.execute_script("window.open('');")
+    driver.switch_to.window(driver.window_handles[0])
+    driver.get("https://www.youtube.com/watch?v=IgEXWRSJbi4")
+
+    time.sleep(20)
+    video_element.pause()
+
+    print(len(driver.window_handles))
+    
+selen()
+
+#pychr()
