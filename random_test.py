c78d8ed227d8281d9a8e6dff5272b0e9116c684f
jopopapi <106134415+jopopapi@users.noreply.github.com>
2024-05-14 12:00:42 +0900

    video player

--- random_test.py
+++ random_test.py
@@ -3,6 +3,80 @@ import face_recognition
 import time
 import tkinter as tk
 from tkinter import messagebox
+from moviepy.editor import VideoFileClip
+import time
+import pygame
+from pyvidplayer2 import Video
+
+def playVideo1():
+
+    video_path = "nba.mp4"
+
+    pygame.init()
+    pygame.display.set_caption(video_path)
+    # Define the video file path
+    
+
+    # Load the video clip
+    clip = VideoFileClip(video_path)
+
+    # Play the video
+    clip = clip.subclip(0, 10) 
+    clip.preview(fps= 60)  # This will start playing the vide
+    pygame.quit()
+
+
+def playVideo2():
+
+    # create video object
+
+    vid = Video("nba.mp4")
+
+    win = pygame.display.set_mode(vid.current_size)
+    pygame.display.set_caption(vid.name)
+
+
+    while vid.active:
+        key = None
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                vid.stop()
+            elif event.type == pygame.KEYDOWN:
+                key = pygame.key.name(event.key)
+        
+        if key == "q":
+            vid.stop()
+        if key == "r":
+            vid.restart()           #rewind video to beginning
+        elif key == "p":
+            vid.toggle_pause()      #pause/plays video
+        elif key == "m":
+            vid.toggle_mute()       #mutes/unmutes video
+        elif key == "right":
+            vid.seek(15)            #skip 15 seconds in video
+        elif key == "left":
+            vid.seek(-15)           #rewind 15 seconds in video
+        elif key == "up":
+            vid.set_volume(1.0)     #max volume
+        elif key == "down":
+            vid.set_volume(0.0)     #min volume
+        elif key == "1":
+            vid.set_speed(1.0)      #regular playback speed
+        elif key == "2":
+            vid.set_speed(2.0)      #doubles video speed
+
+        # only draw new frames, and only update the screen if something is drawn
+        
+        if vid.draw(win, (0, 0), force_draw=False):
+            pygame.display.update()
+
+        pygame.time.wait(16) # around 60 fps
+
+    # close video when done
+
+    vid.close()
+    pygame.quit()
+
 
 def multiVideo():
 
@@ -42,3 +116,5 @@ def multiVideo():
     cap1.release()
     #cap2.release()
     cv2.destroyAllWindows()
+
+playVideo2()
