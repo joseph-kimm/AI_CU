e34f6d9e8d97fdf3b96bcd23631b54f340a25175
Joseph Kim <106134415+joseph-kimm@users.noreply.github.com>
2024-08-14 13:17:00 +0900

    Update install.bat

--- install.bat
+++ install.bat
@@ -16,7 +16,7 @@ REM Upgrade pip
 py -m pip install --upgrade pip
 
 REM Download Dlib
-curl -o dlib-19.24.1-cp311-cp311-win_amd64.whl https://github.com/z-mahmud22/Dlib_Windows_Python3.x/blob/main/dlib-19.24.1-cp311-cp311-win_amd64.whl
+curl -L -o dlib-19.24.1-cp311-cp311-win_amd64.whl https://github.com/z-mahmud22/Dlib_Windows_Python3.x/blob/main/dlib-19.24.1-cp311-cp311-win_amd64.whl
 
 REM Install Dlib
 pip install dlib-19.24.1-cp311-cp311-win_amd64.whl
@@ -24,8 +24,15 @@ pip install dlib-19.24.1-cp311-cp311-win_amd64.whl
 REM Install required libraries
 pip install -r requirements.txt
 
+REM Install FFMPEG
+curl -L -o ffmpeg-release-essentials.zip https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip
+tar -xf ffmpeg-release-essentials.zip
+set PATH=%PATH%;C:\Users\user\Downloads\AI_CU-main\AI_CU-main\ffmpeg-7.0.2-essentials_build\bin
+
 REM Cleanup
 del python-3.11.9-amd64.exe
+del dlib-19.24.1-cp311-cp311-win_amd64.whl
+del ffmpeg-release-essentials.zip
 
 echo Installation complete.
 pause
