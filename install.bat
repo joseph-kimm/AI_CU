67724a02e7a868ffa0c71f15fffc616c668e6753
Joseph Kim <106134415+joseph-kimm@users.noreply.github.com>
2024-08-14 12:47:21 +0900

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,22 +1,31 @@
 @echo off
 
-REM Specify the desired Python version
-set PYTHON_VERSION=3.11
-
 REM Download Python installer
-curl -o python-installer.exe https://www.python.org/ftp/python/%PYTHON_VERSION%/python-%PYTHON_VERSION%-amd64.exe
+curl -o python-3.11.9-amd64.exe https://www.python.org/ftp/python/3.11.9/python-3.11.9-amd64.exe
 
 REM Install Python
-python-installer.exe /quiet InstallAllUsers=1 PrependPath=1
+python-3.11.9-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
 
 REM Create a virtual environment
-python -m venv venv
+py -m venv ai_cu_env
 
 REM Activate the virtual environment
-call venv\Scripts\activate
+ai_cu_env\Scripts\activate
+
+REM Upgrade pip
+py -m pip install --upgrade pip
+
+REM Download Dlib
+curl -o dlib-19.24.1-cp311-cp311-win_amd64.whl https://github.com/z-mahmud22/Dlib_Windows_Python3.x/blob/main/dlib-19.24.1-cp311-cp311-win_amd64.whl
+
+REM Install Dlib
+pip install dlib-19.24.1-cp311-cp311-win_amd64.whl
+
+REM Install required libraries
+pip install -r requirements.txt
 
 REM Cleanup
-del python-installer.exe
+del python-3.11.9-amd64.exe
 
 echo Installation complete.
 pause
